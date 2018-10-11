e0594f021cef2f5696f35f6fdddfbd33d352bc1d
missasd2 <jma4@students.imperial.edu>
2018-10-11 10:44:29 +0800

    Commit Message

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+//package com.company;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//	// write your code here
+//    }
+//}
+
+
+package com.company;
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.print("I Rule");
+        System.out.print("The world");
+
+     int x = 4;
+     while(x > 0){
+         x = x - 1;
+         System.out.print(x);
+     }
+
+     int z = 27;
+     while(z > 0){
+         z = z -1;
+         System.out.print(z);
+     }
+    }
+}
