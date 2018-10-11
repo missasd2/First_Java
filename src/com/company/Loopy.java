9f24d360155324268a1f34c0d2e090fec932813d
missasd2 <jma4@students.imperial.edu>
2018-10-11 11:09:41 +0800

    Commit Message

---
+++ src/com/company/Loopy.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Loopy {
+    public static void main(String[] args){
+        int x = 1;
+        System.out.print("Before the Loop");
+        while (x<4){
+            System.out.print("In the Loop");
+            System.out.print("Value of x is " + x);
+            x = x + 1;
+        }
+        System.out.print("This is after the loop");
+    }
+}
